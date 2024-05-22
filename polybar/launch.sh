ad5c0fe570a87ccccfcf064e94345d9fb69e30aa
TheCarrotOfDoom <puttefnask70@gmail.com>
2024-05-22 08:16:53 +0200

    has three bars

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,8 +7,16 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 #Launch bar1 and bar2
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload example &
+        MONITOR=$m polybar --reload root &
+        MONITOR=$m polybar --reload back &
+        MONITOR=$m polybar --reload left &
+        MONITOR=$m polybar --reload right &
+        MONITOR=$m polybar --reload center &
     done
 else
-    polybar --reload example&
+    polybar --reload root &
+    polybar --reload back &
+    polybar --reload left &
+    polybar --reload right &
+    polybar --reload center &
 fi
