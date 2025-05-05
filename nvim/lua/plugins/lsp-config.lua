f13d1e6fcf6713a3f2aeebd61a63679bf8deff76
TheCarrotOfDoom <puttefnask70@gmail.com>
2025-05-05 08:33:21 +0200

    Changed polybar

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -13,7 +13,7 @@ return {
         },
         config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "texlab"}
+        ensure_installed = { "lua_ls", "clangd", "texlab", "pyright"}
       })
     end
   },
@@ -26,6 +26,9 @@ return {
             local lspconfig = require("lspconfig")
 
             lspconfig.lua_ls.setup({
+            capabilities = capabilities
+            })
+            lspconfig.pyright.setup({
             capabilities = capabilities
             })
             --[[lspconfig.clangd.setup({
